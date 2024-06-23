a8d21a98e0762ca97f3a69400dd1e4fab276a503
Grahp <grahp@grahp.org>
2024-06-23 09:32:22 -0400

    Update Tmux Popup Script

--- tmux/.config/tmux/popup.sh
+++ tmux/.config/tmux/popup.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-tmux popup -w 80% -h 80% -E "tmux new-session -c '#{pane_current_path}'\; set-option -g status off"
+current_session=$(tmux display-message -p '#S')
+popup_session="${current_session}-popup"
+session_exists=$(tmux ls | grep -c "$popup_session")
+
+if [[ $current_session == *"-popup" ]]; then
+    tmux detach-client
+else
+    if [ "$session_exists" -eq 0 ]; then
+        tmux popup -w 80% -h 80% -E "tmux new-session -s $popup_session -c '#{pane_current_path}'\; set-option -g status off"
+    else
+        tmux popup -w 80% -h 80% -E "tmux attach-session -t $popup_session"
+    fi
+fi
