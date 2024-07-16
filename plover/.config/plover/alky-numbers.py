6d16134e1619e93b111d77e6492e97674377a3af
Grahp <grahp@grahp.org>
2024-07-16 00:41:32 -0400

    nomblers

--- plover/.config/plover/alky-numbers.py
+++ plover/.config/plover/alky-numbers.py
@@ -1,4 +1,13 @@
 LONGEST_KEY = 1
 
 def lookup(CHORD: tuple[str]):
-    return "test"
+    STROKE = CHORD[0]
+
+    match = re.fullmatch(r'(#?)(S?T?K?P?W?H?R?)(A?O?E?U?)([-*]?)([FRPB])([LGTS]*)', STROKE)
+    if match is None:
+        raise KeyError
+
+
+    (NUMBER, LEFT_HAND, VOWELS, SEPERATOR, NUMPAD0, NUMPAD1) = match.groups()
+
+
