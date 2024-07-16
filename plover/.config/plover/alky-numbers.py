d1538306a564654a0fc96a174b7c5d6af5bd243b
Grahp <grahp@grahp.org>
2024-07-16 00:49:11 -0400

    los numeros

--- plover/.config/plover/alky-numbers.py
+++ plover/.config/plover/alky-numbers.py
@@ -1,13 +1,21 @@
+import re
+
 LONGEST_KEY = 1
+
+NUMBER_KEY = "#"
+UNFORMATTED_NUMBER = "#TPH"
 
 def lookup(CHORD: tuple[str]):
     STROKE = CHORD[0]
 
-    match = re.fullmatch(r'(#?)(S?T?K?P?W?H?R?)(A?O?E?U?)([-*]?)([FRPB])([LGTS]*)', STROKE)
-    if match is None:
+    if NUMBER_KEY not in STROKE:
         raise KeyError
 
+    MATCH = re.fullmatch(r'(#?S?T?K?P?W?H?R?)(A?O?E?U?)([-*]?)([FRPB])([LGTS]*)', STROKE)
+    if MATCH is None:
+        raise KeyError
 
-    (NUMBER, LEFT_HAND, VOWELS, SEPERATOR, NUMPAD0, NUMPAD1) = match.groups()
+    (LEFT_HAND, VOWELS, SEPERATOR, NUMPAD0, NUMPAD1) = MATCH.groups()
 
-
+    if UNFORMATTED_NUMBER in STROKE:
+        return "oh it's in there all right"
